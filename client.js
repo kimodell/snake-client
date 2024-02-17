3eab886f5566138f822ee9bb284ce37d5e69724e
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-17 18:02:24 -0500

    create client.js to contain connect function. Exported connect function to play.js

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+//establishes connection with gamer server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.on("connect", () => {
+    //code that does something when the connection is first established
+    console.log("Successfully connected to server!");
+  });
+
+  //handle data from the server code and display the message to the console
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("connecting ...");
+connect();
+
+//export to play.js
+module.exports = {
+  connect,
+}
--- play.js
+++ play.js
@@ -1,28 +1,4 @@
-const net = require("net");
+//import conenct function from client.js
+const { connect } = require("./client");
 
-//establishes connection with gamer server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  conn.on("connect", () => {
-    //code that does something when the connection is first established
-    console.log("Successfully connected to server!");
-  });
-
-  //handle data from the server code and display the message to the console
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-
-  //interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("connecting ...");
-connect();
+connect;
