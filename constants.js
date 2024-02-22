c633b133e2c26de125f87bda0b0af2fd89776e18
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-21 19:15:34 -0500

    moved keyMap object to constants.js to clean up input.js. Exported to constants to input.js

--- constants.js
+++ constants.js
@@ -1,10 +1,24 @@
 //module containing constant values which are exported for use in the rest of the app
 
-const IP = "localhost"
+const IP = "localhost";
 
-const PORT = 50541
+const PORT = 50541;
+
+//Object storing mutiple mapped keys 
+
+const keyMap = {
+  'w': "Move: up",
+  'a': "Move: left",
+  's': "Move: down",
+  'd': "Move: right",
+  'g': "Say: GG WP",
+  'e': "Say: Sorry N00b",
+  'r': "Say: Maybe practice first",
+  'f': "Say: GLHF",
+};
 
 module.exports = {
   IP,
   PORT,
+  keyMap
 };
--- input.js
+++ input.js
@@ -1,17 +1,5 @@
-//Object storing mutiple mapped keys 
-
-const keyMap = {
-  'w': "Move: up",
-  'a': "Move: left",
-  's': "Move: down",
-  'd': "Move: right",
-  'g': "Say: GG WP",
-  'e': "Say: Sorry N00b",
-  'r': "Say: Maybe practice first",
-  'f': "Say: GLHF",
-}
-
-//function to handle keyboard inputs by user, including exiting the game with ctrl + c and all actions mapped to keys in the keyMap object
+const { keyMap } = require("./constants");
+//function to handle keyboard inputs by user, including exiting the game with ctrl + c and all actions mapped to keys in the keyMap object from constants.js
 
 const handleUserInput = function(data) {
   if (data === '\u0003') {
@@ -38,4 +26,4 @@ const setupInput = (conn) => {
 //export to play.js
 module.exports = {
   setupInput,
-}
+};
